fe6a683ced7c9951ec95cf988afaa52d9117c415
WilsonWu <wilson.wu@kantar.com>
2019-07-18 15:36:36 +0800

    uwsgi

--- server/SpeechToText/SpeechToText/wsgi.py
+++ server/SpeechToText/SpeechToText/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpeechToText.settings')
 
 application = get_wsgi_application()
+sys.path.append("/root/Project/SpeechToText/server/SpeechToText/")
